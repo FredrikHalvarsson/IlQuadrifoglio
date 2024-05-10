d7100a7bc6a8edf55f33857c957bff93397815cb
Fredrik Halvarsson <Fredrik.Halvarsson@live.se>
2024-05-10 11:24:01 +0200

    Add initial models

---
+++ Models/OrderProduct.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IlQuadrifoglio.Models
+{
+    public class OrderProduct
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int FkOrderId { get; set; }
+
+        [ForeignKey("Product")]
+        public int FkProductId { get; set; }
+        public Product Product { get; set; }
+        public IEnumerable<Ingredient>? Ingredients { get; set; }
+    }
+}
