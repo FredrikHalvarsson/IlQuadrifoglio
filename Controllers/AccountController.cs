17ae83842f880d4e657f436257d48bd923f8defe
Fredrik Halvarsson <Fredrik.Halvarsson@live.se>
2024-05-15 11:16:31 +0200

    Login through api

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using IlQuadrifoglio.Models;
+using IlQuadrifoglio.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IlQuadrifoglio.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly APIService _apiService;
+
+        public AccountController(APIService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        [HttpGet]
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _apiService.LoginAsync(model.Username, model.Password);
+                if (result)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Register() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _apiService.RegisterAsync(model.Username, model.Password, model.Email);
+                if (result)
+                {
+                    return RedirectToAction("Login");
+                }
+                ModelState.AddModelError(string.Empty, "Registration failed.");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IlQuadrifoglio.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
