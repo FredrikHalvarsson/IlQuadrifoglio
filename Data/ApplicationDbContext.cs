7577d78b0563ce505a0a3b877886efc2336c40ef
Glind99 <gustav.lind.net23@edu.edugrade.se>
2024-05-22 11:14:20 +0200

    Fixade login & register

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -4,16 +4,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace IlQuadrifoglio.Data
 {
-    //public class ApplicationDbContext : IdentityDbContext
-    //{
-    //    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-    //        : base(options)
-    //    {
-    //    }
-    //    public DbSet<ApplicationUser> Users { get; set; }
-    //    public DbSet<Order> Orders { get; set; }
-    //    public DbSet<Product> Products { get; set; }
-    //    public DbSet<Ingredient> Ingredients { get; set; }
-    //    public DbSet<OrderProduct> OrderProducts { get; set; }
-    //}
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -13,9 +13,9 @@ namespace IlQuadrifoglio
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            //builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlServer(connectionString));
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
             //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
